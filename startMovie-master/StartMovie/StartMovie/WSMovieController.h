ebae47581ebad51c8e3f4017b5d28038f13b911a
Liuchuqni <chunqi.liu@cloudnapps.com>
2016-11-22 10:56:01 +0800

    movie start

---
+++ startMovie-master/StartMovie/StartMovie/WSMovieController.h
@@ -0,0 +1,17 @@
+//
+//  WSMovieController.h
+//  StartMovie
+//
+//  Created by Charlotte on 16/8/29.
+//  Copyright © 2016年 sinfotek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSMovieController : UIViewController
+
+@property(nonatomic,strong)NSURL *movieURL;
+@property id vc;
+
+
+@end
