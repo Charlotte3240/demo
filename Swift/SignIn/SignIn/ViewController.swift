dac6f5ec9f70a70333ccc177717b454bb9ababa3
Charlotte <momo_sad@hc-nsqk.com>
2020-09-24 09:27:10 +0800

    feat: 🎸 add gpx file

--- Swift/SignIn/SignIn/ViewController.swift
+++ Swift/SignIn/SignIn/ViewController.swift
@@ -9,17 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var manager : CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.requestAlwaysAuthorization()
+        return manager
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let location = CLLocationCoordinate2D.init(latitude: 31.239028, longitude: 121.511093)
+        self.manager.delegate = self
+        self.manager.startUpdatingLocation()
         
+    }
+    
+    
+    func convertLocation() {
+        //船舶大厦121.511064,31.23859
+        let location = CLLocationCoordinate2D.init(latitude: 31.23859, longitude: 121.511064)
+        //巨洋大厦121.534119,31.241564
+//        let location = CLLocationCoordinate2D.init(latitude: 31.241564, longitude: 121.534119)
+
         let convertLocatin = ConvertLocation.gcj02(toWgs84: location)
         
         print(convertLocatin)
-        
+
     }
 
+
+}
+
+extension ViewController : CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        print(locations)
+    }
 
 }
 
