4abf746f9a1829c62133c26b11eb7fcd240cb3e7
Liuchunqi3240 <momo_sad@hc-nsqk.com>
2021-10-29 18:49:31 +0800

    feat: 🎸 add golang Two-way certificate verify

--- golang/grpc-client-demo/main.go
+++ golang/grpc-client-demo/main.go
@@ -2,15 +2,60 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"io/ioutil"
 	"log"
 	"nsqk.com/rpc/services"
 )
 
 func main() {
+	useClientPemFile()
+}
 
+func useClientPemFile(){
+	// 和server端一样，先创建证书池
+	cert, err := tls.LoadX509KeyPair("cert/client.pem","cert/client.key")
+	if err!= nil{
+		log.Println("加载client pem, key 失败",err)
+	}
+
+	certPool := x509.NewCertPool()
+	caFile ,err :=  ioutil.ReadFile("cert/ca.pem")
+	if err!= nil{
+		log.Println("加载ca失败",err)
+	}
+	certPool.AppendCertsFromPEM(caFile)
+
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},// 放入客户端证书
+		ServerName: "localhost", //证书里面的 commonName
+		RootCAs: certPool, // 根证书池
+	})
+
+	conn, err := grpc.Dial(":2333", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := services.NewHelloWorldClient(conn)
+	replay, err := client.HelloRPC(context.Background(), &services.SayHelloRequest{
+		Name: "hello world name",
+	})
+	if err != nil {
+		fmt.Println("rpc error:", err)
+	} else {
+		fmt.Println("result message :", replay.Message)
+	}
+
+}
+
+
+func useServerPemFile(){
 	// 加载证书，创建credentials
 	// 这里的serverNameOverride 要填写服务端的地址
 	creds ,err := credentials.NewClientTLSFromFile("keys/server.pem","127.0.0.1")
@@ -34,5 +79,4 @@ func main() {
 	} else {
 		fmt.Println("result message :", replay.Message)
 	}
-
 }
--- golang/grpc-demo/server.go
+++ golang/grpc-demo/server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -12,19 +15,70 @@ import (
 )
 
 func main() {
-	startGRPCHttpServer()
+	//startGRPCHttpServer()
+	//startGRPCServer()
+	startTwoWayCertGRPCServer()
+	fmt.Println("ddd")
 }
 
-func startGRPCHttpServer(){
+// startTwoWayCertGRPCServer 双向证书验证
+func startTwoWayCertGRPCServer() {
+	// 加载双向验证证书形式
+	// 使用tls 进行加载  key pair
+	cert, err := tls.LoadX509KeyPair("cert/server.pem", "cert/server.key")
+	if err != nil {
+		log.Println("tls 加载x509 证书失败", err)
+	}
+	// 创建证书池
+	certPool := x509.NewCertPool()
+
+	// 向证书池中加入证书
+	cafileBytes, err := ioutil.ReadFile("cert/ca.pem")
+	if err != nil {
+		log.Println("读取ca.pem证书失败", err)
+	}
+	// 加载客户端证书
+	//certPool.AddCert()
+
+	// 加载证书从pem 文件里面
+	certPool.AppendCertsFromPEM(cafileBytes)
+
+	// 创建credentials 对象
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},        //服务端证书
+		ClientAuth:   tls.RequireAndVerifyClientCert, // 需要并且验证客户端证书
+		ClientCAs:    certPool,                       // 客户端证书池
+
+	})
+
+	lis, err := net.Listen("tcp", ":2333")
+	if err != nil {
+		fmt.Println("监听端口失败 err:", err)
+	}
+	s := grpc.NewServer(grpc.Creds(creds))
+	services.RegisterHelloWorldServer(s, new(services.HelloService))
+
+	reflection.Register(s)
+
+	err = s.Serve(lis)
+	fmt.Println("gRPC starting")
+	if err != nil {
+		fmt.Println("启动grpc失败 err:", err)
+	} else {
+		fmt.Println("gRPC started")
+	}
+}
+
+func startGRPCHttpServer() {
 	// 1. 加载证书
-	creds ,err := credentials.NewServerTLSFromFile("keys/server.pem","keys/server.key")
-	if err !=nil{
-		log.Fatalln("加载证书失败",err)
+	creds, err := credentials.NewServerTLSFromFile("keys/server.pem", "keys/server.key")
+	if err != nil {
+		log.Fatalln("加载证书失败", err)
 		return
 	}
 
 	s := grpc.NewServer(grpc.Creds(creds))
-	services.RegisterHelloWorldServer(s,new(services.HelloService))
+	services.RegisterHelloWorldServer(s, new(services.HelloService))
 	// 注册grpc server
 	reflection.Register(s)
 
@@ -32,11 +86,11 @@ func startGRPCHttpServer(){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		// 打印req
-		log.Println(request.Proto,request.Header,request.Method)
-		s.ServeHTTP(writer,request)
+		log.Println(request)
+		s.ServeHTTP(writer, request)
 	})
 	httpServer := &http.Server{
-		Addr:":2333",
+		Addr:    ":2333",
 		Handler: mux,
 	}
 	// gRPC requires HTTP/2
@@ -44,32 +98,28 @@ func startGRPCHttpServer(){
 	//httpServer.ListenAndServe()
 
 	// 下面的方式启动，就可以通过https来进行访问了
-	httpServer.ListenAndServeTLS("keys/server.pem","keys/server.key")
-
-
+	httpServer.ListenAndServeTLS("keys/server.pem", "keys/server.key")
 
 }
 
-
-func startGRPCServer(){
+func startGRPCServer() {
 	// 加载证书
-	creds,err := credentials.NewServerTLSFromFile("keys/server.pem","keys/server.key")
-	if err != nil{
-		log.Fatalln("加载证书失败",err)
+	creds, err := credentials.NewServerTLSFromFile("keys/server.pem", "keys/server.key")
+	if err != nil {
+		log.Fatalln("加载证书失败", err)
 	}
 
-	lis , err := net.Listen("tcp",":2333")
+	lis, err := net.Listen("tcp", ":2333")
 	if err != nil {
-		fmt.Println("监听端口失败 err:",err)
+		fmt.Println("监听端口失败 err:", err)
 	}
 	s := grpc.NewServer(grpc.Creds(creds))
-	services.RegisterHelloWorldServer(s,new(services.HelloService))
+	services.RegisterHelloWorldServer(s, new(services.HelloService))
 
 	reflection.Register(s)
 
 	err = s.Serve(lis)
-	if err != nil{
-		fmt.Println("启动grpc失败 err:",err)
+	if err != nil {
+		fmt.Println("启动grpc失败 err:", err)
 	}
 }
-
